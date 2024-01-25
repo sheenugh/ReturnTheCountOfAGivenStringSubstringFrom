871f13de1f720cff90210af4dfc0b2b5b35086df
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 10:42:37 +0800

    done na: nagworked

--- displaying_how_many_times_a_substring_appear_from_the_string.py
+++ displaying_how_many_times_a_substring_appear_from_the_string.py
@@ -1,5 +1,10 @@
 
 
 # ----- PSEUDO CODE -----
-# - Actual Code
+# - ACTUAL CODE
+# - Variable
+given_statement = "Emma is good developer. Emma is a writer"
+
 # - Performing the count
+emma_count = given_statement.count("Emma")
+print("Emma appeared " + str(emma_count) + " times.")
